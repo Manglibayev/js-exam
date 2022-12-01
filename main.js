1915b771825baa8b169ffe035eda838c94ec7682
RahimboyData <barleybarley76gmail.com>
2022-12-01 17:43:17 +0500

    second commit

---
+++ main.js
@@ -0,0 +1,85 @@
+//Savolga javoblar
+
+/*
+
+1.var, let , const 
+var va letni keyin chaqirib ozgartirsa boladi constni esa ozgartirib bolmaydi
+
+2."string", number
+
+3.increment operatori javobga 1 ni qoshadi 
+  decrement operatori javobdan 1 ni ayirib tashlaydi
+
+4.string + number = string
+
+5. && bu "va"ni anglatadi if(typeof(a)==="number" && typeof(b)==="number")
+   || bu "yoki"ni anglatadi
+
+6.if else bu boolean
+    if else ishlashi
+    if(a>b){
+        console.log("Hello") //agar a b dan katta bolsa "Hello" yozuvi chiqsin 
+    }
+    else{
+        console.log("World") //aks holda "World" yozuvi chiqsin
+    }
+
+7.    if(a>b){
+        console.log("Hello")
+    }
+    else if(a===b){
+        console.log("a bilan b teng")
+    }
+    else{
+        console.log("World")
+    }
+
+8.var arr=["Maqsud", "Dovud", "Javohir", "Juratbek"]
+  console.log(arr[2]); //Javohirni consoleda chiqaradi
+
+9.  object
+let info = {
+    name:"Amir",
+    lastName:"Manglibayev",
+    Age:16,
+    adress:{
+        street:"Proyeqtnaya"
+        house: 63
+    }
+}
+
+10.method - bu obyektlarda bajariladigan harakat.
+JavaScript usuli funksiya taʼrifini oʻz ichiga olgan xususiyatdir.
+
+
+
+
+//Misollar
+
+1.function calc(a,b) {
+    console.log(a+b);
+}
+    console.log(calc(22,8))
+
+
+2.function year(a) {
+    console.log(2022-a)
+}
+console.log(year(16));
+
+
+3.function Func(a,b) {
+    if(typeof(a)=== "string"){
+        console.log("son emas");
+    }
+    else if(typeof(b)==="string"){
+        console.log("son emas");
+    }
+    else{
+        console.log(a+b);
+    }
+}
+console.log(Func("sa",13));
+
+
+*/
